e12c39b4328ec657bd085c198cf80cb75ef1d3f1
Ehsan Faramarzi <netpro@email.com>
2024-04-30 15:38:59 +0200

    redundant

--- NiC.Authentication.Abstractions/IApiCommunicater.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NiC.Authentication.Abstractions;
-
-public interface IApiCommunicater
-{
-    Task<HttpResponseMessage> PostAsJsonAsync<TValue>(string configKey, string action, TValue value);
-
-    Task<HttpResponseMessage> GetAsync(string configKey, string action);
-}
